7c4be205105036f8a5f8733194a7d928254441d6
topoescobar <ivan90ge@gmail.com>
2023-06-01 00:00:30 -0300

    login fcns

--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,46 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-   const onChange = () => {
+
+   const [user, saveUser] = useState({
+      email: '',
+      password: ''
+   })
+
+
+   const changeFn = (e) => {
+      //esta fn toma copia de lo que ya se inreso en user (email o passw), y le agrega el otro campo dinamicamente usando los atributos name y value
+      saveUser({
+         ...user,
+         [e.target.name]: e.target.value
+      })
    }
 
-   const onSubmit = () => { }
+   const submitFn = (e) => {
+      e.preventDefault()
+   }
 
    return (
-      <div className='form-usuario'>
-         <div className='contenedor-form sombra-dark'>
+      <div className='user_form'>
+         <div className='containter_form dark_shadow'>
             <h1> Iniciar sesion </h1>
 
-            <form onSubmit={onSubmit} >
+            <form onSubmit={submitFn} >
 
-               <div className="campo-form">
+               <div className="form_field">
                   <label htmlFor="email">Email</label>
                   <input
                      type="email"
                      id="email"
                      name="email"
                      placeholder="Tu Email"
-                     //  value={email}
-                     onChange={onChange}
+                     value={user.email}
+                     onChange={changeFn}
                   />
                </div>
 
-               <div className="campo-form">
+               <div className="form_field">
                   <label htmlFor="password">Password</label>
                   <input
                      type="password"
                      id="password"
                      name="password"
                      placeholder="Tu Password"
-                     //  value={password}
-                     onChange={onChange}
+                     value={user.password}
+                     onChange={changeFn}
                   />
                </div>
 
-               <div className="campo-form">
-                  <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesión" />
+               <div className="form_field">
+                  <input type="submit" className="btn btn_primary btn-block" value="Iniciar Sesión" />
                </div>
             </form>
+
+            <div className='enlace-cuenta'>
+               <p> No tenes cuenta? </p>
+               <Link to={'/new-acount'}>
+                  Crear nueva cuenta
+               </Link>
+            </div>
 
          </div>
       </div>
