aa9ffc374ddb1e55b6a878492f36e36d40973378
topoescobar <ivan90ge@gmail.com>
2023-06-04 12:29:57 -0300

    navbar y tareas

---
+++ src/components/tasks/task.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Task = ({task}) => { //recibo la tarea como props de tasklis
+  return (
+    <li className="tarea sombra">
+    <p>{tarea.nombre} </p>
+
+    <div className="estado">
+        {tarea.estado 
+        ?  
+            (
+                <button
+                    type="button"
+                    className="completo"
+                    onClick={() => cambiarEstado(tarea)}
+                >Completo</button>
+            )
+        : 
+            (
+                <button
+                    type="button"
+                    className="incompleto"
+                    onClick={() => cambiarEstado(tarea)}
+                >Incompleto</button>
+            )
+        }
+    </div>
+
+    <div className="acciones">
+        <button 
+            type="button"
+            className="btn btn-primario"
+            onClick={() => seleccionarTarea(tarea) }
+        >Editar</button>
+
+        <button
+            type="button"
+            className="btn btn-secundario"
+            onClick={() => tareaEliminar(tarea._id)}
+        >Eliminar</button>
+    </div>
+</li>  )
+}
+
+export default Task
